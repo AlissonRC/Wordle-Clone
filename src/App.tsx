c0d523a0a90948ddb70a7a964e231abda79589f9
AlissonRC <alissonrdacruz@gmail.com>
2022-06-30 01:19:08 -0300

    changes on ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import BasicGrid from './Grid'
-import Teclado from './TecladoModelo'
-import { palavraRandom, getAllPalavras } from './listaDePalavras'
+import { useState } from "react";
+import "./App.css";
+import BasicGrid from "./Grid";
+import Teclado from "./TecladoModelo";
+import { palavraRandom, getAllPalavras } from "./listaDePalavras";
 
 let palavraCorreta = palavraRandom();
-
 function App() {
-  const [isInvalidWord, setIsInvalidWord] = useState(false)
-  const [isWordShort, setIsWordShort] = useState(false)
-  const [isGameOver, setIsGameOver] = useState(false)
-  const [isGameWin, setIsGameWin] = useState(false)
-  const [linha, setLinha] = useState(0)
-  const [coluna, setColuna] = useState(0)
-  const [tentativa, setTentativa] = useState([" ", " ", " ", " ", " "])
+  const [isInvalidWord, setIsInvalidWord] = useState(false);
+  const [isWordShort, setIsWordShort] = useState(false);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [isGameWin, setIsGameWin] = useState(false);
+  const [linha, setLinha] = useState(0);
+  const [coluna, setColuna] = useState(0);
+  const [tentativa, setTentativa] = useState([" ", " ", " ", " ", " "]);
   const [matriz, setMatriz] = useState([
     [" ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " "],
@@ -21,86 +20,71 @@ function App() {
     [" ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " "],
-  ])
+  ]);
 
-
-  function validateValidWord(){
-    let isValidWord = false
-    getAllPalavras().map((palavra)=>{
-      if(palavra.toUpperCase() === tentativa.toString().replaceAll(',','')){
-        isValidWord = true   
-
+  function validateValidWord() {
+    let isValidWord = false;
+    getAllPalavras().map((palavra) => {
+      if (palavra.toUpperCase() === tentativa.toString().replaceAll(",", "")) {
+        isValidWord = true;
       }
-     
-    })
-    return isValidWord
+    });
+    return isValidWord;
   }
-
 
   function handleKeyPress(tecla: string) {
     //const index = tentativa.findIndex((item)=> item===" ");
 
     if (tecla === "DELETE [X]" && coluna > 0 && isGameOver === false) {
-      tentativa[coluna - 1] = " "
-      setTentativa([...tentativa])
-    
+      tentativa[coluna - 1] = " ";
+      setTentativa([...tentativa]);
+
       matriz[linha] = tentativa;
       setMatriz([...matriz]);
-      setColuna(coluna - 1)
-    }
-    else if (coluna <= 4 && tecla.length === 1) {
+      setColuna(coluna - 1);
+    } else if (coluna <= 4 && tecla.length === 1) {
       tentativa[coluna] = tecla;
       setTentativa([...tentativa]);
       matriz[linha] = tentativa;
       setMatriz([...matriz]);
 
       let temp = coluna;
-      temp++
+      temp++;
       setColuna(temp);
-    
-    }
-
-    else if (tecla === "ENTER" && coluna === 5 ) {
-
-      if (!validateValidWord()){
-        setIsInvalidWord(true)
-        return
+    } else if (tecla === "ENTER" && coluna === 5) {
+      if (!validateValidWord()) {
+        setIsInvalidWord(true);
+        return;
       }
-     setIsInvalidWord(false)
+      setIsInvalidWord(false);
 
       setLinha(linha + 1);
-      setIsWordShort(false)
+      setIsWordShort(false);
       if (validaTentativa(tentativa) === false) {
         if (linha < 5) {
           setTentativa([" ", " ", " ", " ", " "]);
           setColuna(0);
         } else {
-          setIsGameOver(true)
-       
+          setIsGameOver(true);
         }
       } else {
-        setIsGameWin(true)
-   
+        setIsGameWin(true);
       }
-    }
-    else if (tecla === "ENTER" && coluna < 5) {
-      setIsWordShort(true)
+    } else if (tecla === "ENTER" && coluna < 5) {
+      setIsWordShort(true);
 
       setTimeout(() => {
-        setIsWordShort(false)
+        setIsWordShort(false);
       }, 7000);
-
     }
-
   }
 
   function validaTentativa(palavra: string[]) {
-    let palavraString = ''
+    let palavraString = "";
     palavra.map((letra, index) => {
-      
-      palavraString = palavraString.concat(letra)
-    })
- 
+      palavraString = palavraString.concat(letra);
+    });
+
     if (palavraString === palavraCorreta.toUpperCase()) {
       return true;
     } else {
@@ -109,8 +93,8 @@ function App() {
   }
 
   function novoJogo() {
-    setIsGameOver(false)
-    setIsGameWin(false)
+    setIsGameOver(false);
+    setIsGameWin(false);
     setMatriz([
       [" ", " ", " ", " ", " "],
       [" ", " ", " ", " ", " "],
@@ -118,65 +102,77 @@ function App() {
       [" ", " ", " ", " ", " "],
       [" ", " ", " ", " ", " "],
       [" ", " ", " ", " ", " "],
-    ])
-    setColuna(0)
-    setLinha(0)
-    setTentativa([" ", " ", " ", " ", " "])
-    palavraCorreta = palavraRandom()
+    ]);
+    setColuna(0);
+    setLinha(0);
+    setTentativa([" ", " ", " ", " ", " "]);
+    palavraCorreta = palavraRandom();
   }
 
   return (
     <div className="App">
       {isGameOver && (
-        <div className='game-over'>
-          Não foi dessa vez !
-          <p>A palavra era: {palavraCorreta}</p>
-          <button onClick={() => novoJogo()}>Novo tentativa</button>
+        <div className="game-over">
+          Não foi dessa vez !<p>A palavra era: {palavraCorreta}</p>
+          <button className="new-game-button" onClick={() => novoJogo()}>
+            Novo tentativa
+          </button>
         </div>
       )}
       {isGameWin && (
-        <div className='game-win'>
+        <div className="game-win">
           Acertou !!!
           <p>{palavraCorreta}</p>
-          <button onClick={() => novoJogo()}>Novo Jogo</button>
+          <button className="new-game-button" onClick={() => novoJogo()}>
+            Novo Jogo
+          </button>
         </div>
       )}
-
 
       <header className="App-header">
         <p>CHARADA</p>
         {isWordShort && (
-          <div className='short-word'>
-             Insira uma palavra com 5 letras
-            <button className='close' onClick={()=>{
-              setIsWordShort(false)
-            }}><i className="gg-close"></i></button>
-             
+          <div className="short-word">
+            Insira uma palavra com 5 letras
+            <button
+              className="close"
+              onClick={() => {
+                setIsWordShort(false);
+              }}
+            >
+              <i className="gg-close"></i>
+            </button>
           </div>
         )}
-         {isInvalidWord && (
-          <div className='short-word'>
-             Palavra não encontrada
-            <button className='close' onClick={()=>{
-              setIsInvalidWord(false)
-            }}><i className="gg-close"></i></button>
-             
+        {isInvalidWord && (
+          <div className="short-word">
+            Palavra não encontrada
+            <button
+              className="close"
+              onClick={() => {
+                setIsInvalidWord(false);
+              }}
+            >
+              <i className="gg-close"></i>
+            </button>
           </div>
         )}
         {matriz.map((value, index) => {
           return (
-            <BasicGrid palavra={matriz[index].join("")} palavraCorreta={palavraCorreta} valida={(linha > index)} />
-
-          )
+            <BasicGrid
+              palavra={matriz[index].join("")}
+              palavraCorreta={palavraCorreta}
+              valida={linha > index}
+            />
+          );
         })}
 
         <p>
           <Teclado onKeyPress={handleKeyPress} />
         </p>
       </header>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
